b800f4eb68513306204495cb4e2e29ec6f962e2f
pSavin1337 <p.savin73@gmail.com>
2021-10-12 22:41:09 +0400

    add db helper class

---
+++ app/src/main/java/com/lospollos/songnote/model/DatabaseHelper.kt
@@ -0,0 +1,28 @@
+package com.lospollos.songnote.model
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context?) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(
+            "CREATE TABLE IF NOT EXISTS songs (\n" +
+                    "    id integer PRIMARY KEY AUTOINCREMENT,\n" +
+                    "    name character(100) NOT NULL,\n" +
+                    "    author character(100) NOT NULL,\n" +
+                    "\t  link character(100));\n"
+        )
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS " + DATABASE_NAME)
+        onCreate(db)
+    }
+
+    companion object {
+        private val DATABASE_NAME = "songs.db" // название бд
+        private val DATABASE_VERSION = 1 // версия базы данных
+    }
+}
--- app/src/main/java/com/lospollos/songnote/view/MainActivity.kt
+++ app/src/main/java/com/lospollos/songnote/view/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.lospollos.songnote
+package com.lospollos.songnote.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.lospollos.songnote.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
